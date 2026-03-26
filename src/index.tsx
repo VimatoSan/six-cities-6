1d048cda4976b44ed5f0bd0f361bd1759cbbb5ce
Vimato <monst3k003@gmail.com>
2026-03-26 15:40:59 +0700

    Add store to the App

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App.tsx';
 import { offers } from './mocks/offers.ts';
+import {Provider} from 'react-redux';
+import {store} from './state';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -10,8 +12,10 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App
-      offers={offers}
-    />
+    <Provider store={store}>
+      <App
+        offers={offers}
+      />
+    </Provider>
   </React.StrictMode>
 );
